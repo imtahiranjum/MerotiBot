5c5f631b211bb89c350b45bc1cedbad2bfb97456
Tahir Anjum <imtahiranjum@gmail.com>
2022-05-28 11:50:45 +0500

    Initial Commit

---
+++ Functions/database.py
@@ -0,0 +1,20 @@
+import os
+import pymongo as pymongo
+
+database_access = os.environ['database_access']
+client = pymongo.MongoClient(
+        f"{database_access}")
+database_to_connect = "MerotiBot"
+db = client[database_to_connect]
+print(db)
+server_collection = db["servers"]
+
+
+def get_response(response):
+    channel_got = f"{response}"
+    prettified = channel_got.split(":")
+    prettified = prettified[1].replace("}", "")
+    prettified = prettified.replace(" ", "")
+    prettified = prettified.replace("'", "")
+    prettified = prettified.replace('"', "")
+    return prettified
