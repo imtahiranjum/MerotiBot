1053d21cc591a06b57c2ba9c7fd2db1dc8a2b027
Tahir Anjum <imtahiranjum@gmail.com>
2022-07-14 01:10:33 +0500

    rearranged code

--- Functions/database.py
+++ Functions/database.py
@@ -1,12 +1,11 @@
 import os
 import pymongo as pymongo
 
-database_access = os.getenv('database_access')
-client = pymongo.MongoClient(
-        f"{database_access}")
+client = pymongo.MongoClient(os.getenv('database_access'))
+# print(client)
 database_to_connect = "MerotiBot"
 db = client[database_to_connect]
-print(db)
+# print(db)
 server_collection = db["servers"]
 
 
